7d4dbc48dafe91f6f87db2699241865ae6c82558
Anders Ingemann <aim@orbit.online>
2024-01-30 16:07:41 +0100

    upkg: Remove usage of `upkg root`

--- path_insert_after.sh
+++ path_insert_after.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail; shopt -s inherit_errexit
-source "$(upkg root "${BASH_SOURCE[0]}")/path-tools.sh"
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/path-tools.sh"
 path_insert_after "$@"
